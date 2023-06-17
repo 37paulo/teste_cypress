365b8d2b94b828e4c6fcdb1551dd98ad46325e73
Paulo Junior <paulo.junior@jussi.local>
2023-06-17 16:42:09 -0300

    adicionando teste validando titulo

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,5 +1,17 @@
+///<reference types= "cypress"/>
+
 describe('empty spec', () => {
-  it('passes', () => {
+
+  beforeEach(() => {
     cy.visit('https://www.livelo.com.br/')
+
   })
+
+  it('passes', () => {
+    cy.title().should('eq', 'Livelo | Programa de pontos que você troca por recompensas')
+
+  })
+
+  
+
 })
